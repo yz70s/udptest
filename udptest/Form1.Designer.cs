b5e98084b1307e82473fd80b3822a931c142c0d2
yz70s <yz70s@users.noreply.github.com>
2017-07-07 20:35:58 +0200

    Add project files

---
+++ udptest/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace udptest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tosendaddr = new System.Windows.Forms.TextBox();
+            this.sendcount = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tosendport = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.receivecount = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.toreceiveport = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(22, 129);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(303, 148);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "SEND";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(334, 129);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(303, 148);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "RECEIVE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tosendaddr
+            // 
+            this.tosendaddr.Location = new System.Drawing.Point(176, 9);
+            this.tosendaddr.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tosendaddr.Name = "tosendaddr";
+            this.tosendaddr.Size = new System.Drawing.Size(148, 26);
+            this.tosendaddr.TabIndex = 2;
+            this.tosendaddr.Text = "137.204.48.1";
+            // 
+            // sendcount
+            // 
+            this.sendcount.Enabled = false;
+            this.sendcount.Location = new System.Drawing.Point(176, 89);
+            this.sendcount.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.sendcount.Name = "sendcount";
+            this.sendcount.Size = new System.Drawing.Size(148, 26);
+            this.sendcount.TabIndex = 3;
+            this.sendcount.Text = "1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 14);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "IP Destinazione:";
+            // 
+            // tosendport
+            // 
+            this.tosendport.Location = new System.Drawing.Point(176, 49);
+            this.tosendport.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tosendport.Name = "tosendport";
+            this.tosendport.Size = new System.Drawing.Size(148, 26);
+            this.tosendport.TabIndex = 5;
+            this.tosendport.Text = "23456";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 54);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Porta Destinazione:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 94);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(138, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Numero pacchetti:";
+            // 
+            // receivecount
+            // 
+            this.receivecount.Location = new System.Drawing.Point(488, 89);
+            this.receivecount.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.receivecount.Name = "receivecount";
+            this.receivecount.Size = new System.Drawing.Size(148, 26);
+            this.receivecount.TabIndex = 8;
+            this.receivecount.Text = "200";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(330, 94);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(138, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Numero pacchetti:";
+            // 
+            // toreceiveport
+            // 
+            this.toreceiveport.Location = new System.Drawing.Point(488, 49);
+            this.toreceiveport.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.toreceiveport.Name = "toreceiveport";
+            this.toreceiveport.Size = new System.Drawing.Size(148, 26);
+            this.toreceiveport.TabIndex = 10;
+            this.toreceiveport.Text = "22981";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(334, 54);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(125, 20);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Porta Ricezione:";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Appearance = System.Windows.Forms.Appearance.Button;
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(488, 9);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(91, 30);
+            this.checkBox1.TabIndex = 12;
+            this.checkBox1.Text = "Salva Dati";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(662, 298);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.toreceiveport);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.receivecount);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tosendport);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sendcount);
+            this.Controls.Add(this.tosendaddr);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "fpga udptest";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox tosendaddr;
+        private System.Windows.Forms.TextBox sendcount;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tosendport;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox receivecount;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox toreceiveport;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ udptest/Form1.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace udptest
+{
+    /*public class packetblock
+    {
+        int size;
+        int receivedcount;
+        byte[][] memory;
+        int next;
+        byte[][] packet;
+        int[] used;
+        int[] offset;
+
+        public packetblock(int count)
+        {
+            int a;
+
+            size = count;
+            memory=new byte[count][];
+            for (a = 0; a < count; a++)
+            {
+                memory[a] = new byte[1600];
+            }
+            packet = new byte[count][];
+            used = new int[count];
+            offset = new int[count];
+            clear();
+        }
+
+        void clear()
+        {
+            int a;
+
+            receivedcount = size;
+            for (a = 0; a < size; a++)
+                used[a] = 0;
+            next = 0;
+        }
+
+        byte[] getbuffer()
+        {
+            if (next < size)
+            {
+                next++;
+                return memory[next - 1];
+            }
+            return null;
+        }
+
+        int add(byte[] buffer,int index,int payload_offset)
+        {
+            if (used[index] == 0)
+            {
+                packet[index] = buffer;
+                used[index] = 1;
+                offset[index] = payload_offset;
+                receivedcount--;
+                return 1;
+            }
+            return 0;
+        }
+
+        byte[] get(int index,ref int payload_offset)
+        {
+            if (index < size) 
+            {
+                payload_offset = offset[index];
+                return packet[index];
+            }
+            return null;
+        }
+
+        int filled()
+        {
+            if (receivedcount == 0)
+                return 1;
+            return 0;
+        }
+    }*/
+
+    public partial class Form1 : Form
+    {
+        static int receive_port, receive_count;
+        static int send_port;
+        static string send_address;
+        static int save_data;
+        static string caption_string;
+        static int running;
+        System.Threading.Thread thr;
+        static ulong first, last, now, diff, missing, received, wrap;
+        static ulong recvlast;
+
+        public Form1()
+        {
+            InitializeComponent();
+            running = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Net.Sockets.UdpClient udpc;
+            int ret;
+            int a,c;
+            byte[] sendbuffer;
+
+            sendbuffer = new byte[1400];
+            for (a = 0; a < 100; a++)
+                sendbuffer[a] = (byte)(a & 255);
+            udpc = new System.Net.Sockets.UdpClient(13455);
+
+            udpc.Connect(tosendaddr.Text, System.Convert.ToInt32(tosendport.Text, 10));
+            c=System.Convert.ToInt32(sendcount.Text,10);
+            for (a=0;a < 1/*c*/;a++)
+                ret=udpc.Send(sendbuffer, 100);
+            udpc.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (running == 0)
+            {
+                if (checkBox1.Checked)
+                    save_data = 1;
+                else
+                    save_data = 0;
+                send_port = System.Convert.ToInt32(tosendport.Text, 10);
+                send_address = tosendaddr.Text;
+                receive_port = System.Convert.ToInt32(toreceiveport.Text, 10);
+                receive_count = System.Convert.ToInt32(receivecount.Text, 10);
+                caption_string = string.Empty;
+                thr = new System.Threading.Thread(Form1.DoWork);
+                thr.Start();
+                timer1.Enabled = true;
+                running = 1;
+                button2.Text = "STOP";
+            }
+            else
+            {
+                running = 2;
+                thr.Join();
+                timer1.Enabled = false;
+                running = 0;
+                button2.Text = "RECEIVE";
+            }
+        }
+
+        private static void DoWork()
+        {
+            System.Net.Sockets.UdpClient udpc;
+            System.Net.Sockets.Socket sock;
+            System.Net.IPEndPoint ipe;
+            System.Net.Sockets.UdpClient udpcs;
+            byte[] buffer;
+            int[] receivedblock;
+            int[] differ;
+            int a,c;
+            int timeout;
+            uint sequence_number, sequence_number_next0, sequence_number_next1;
+            int expected;
+            System.IO.FileStream fil;
+            byte[] sendbuffer;
+			uint received_sequence_number, received_packet_number;
+
+            fil = null;
+            if (save_data == 1)
+                fil = new System.IO.FileStream("packets.bin",System.IO.FileMode.Create); //,System.IO.FileAccess.Write,System.IO.FileShare.Read,8*1024*1024,System.IO.FileOptions.WriteThrough);
+            buffer = new byte[1600];
+            differ = new int[257];
+            udpc = new System.Net.Sockets.UdpClient(receive_port);
+            ipe = new System.Net.IPEndPoint(System.Net.IPAddress.Any, 0);
+
+            receivedblock = new int[2];
+            receivedblock[0] = receivedblock[1] = receive_count;
+            sequence_number = 0;
+            sequence_number_next0 = sequence_number + 1;
+            sequence_number_next1 = sequence_number + 2;
+            sendbuffer = new byte[1400];
+            for (a = 0; a < 100; a++)
+                sendbuffer[a] = (byte)(a & 255);
+            sendbuffer[0] = (byte)((receive_count-1) & 255);
+            sendbuffer[1] = (byte)(((receive_count-1) >> 8) & 255);
+            sendbuffer[2] = (byte)((sequence_number) & 255);
+            sendbuffer[3] = (byte)(((sequence_number) >> 8) & 255);
+            sendbuffer[4] = (byte)(((sequence_number) >> 16) & 255);
+            sendbuffer[5] = (byte)(((sequence_number) >> 24) & 255);
+            sendbuffer[6] = 0;
+            udpcs = new System.Net.Sockets.UdpClient(13455);
+            udpcs.Connect(send_address, send_port);
+            //udpcs.Connect("137.204.48.1", 23456);
+            //udpc.Connect("10.0.0.194", 23455);
+            sock = udpc.Client;
+            sock.SetSocketOption(System.Net.Sockets.SocketOptionLevel.Socket, System.Net.Sockets.SocketOptionName.ReceiveBuffer, 4 * 1024 * 1024);
+            c = receive_count;
+            first = 1;
+            last = 0;
+            wrap = 0;
+            missing = 0;
+            received = 0;
+            recvlast = 0;
+            while (true)
+            {
+                /*receivedblock[0] = receivedblock[1];
+                receivedblock[1] = receive_count;*/
+                sendbuffer[2] = (byte)((sequence_number) & 255);
+                sendbuffer[3] = (byte)(((sequence_number) >> 8) & 255);
+                sendbuffer[4] = (byte)(((sequence_number) >> 16) & 255);
+                sendbuffer[5] = (byte)(((sequence_number) >> 24) & 255);
+                a = udpcs.Send(sendbuffer, 7);
+                first = 1;
+                timeout = 1;
+                expected = 0;
+                //while (receivedblock[0] != 0)
+                while (expected < receive_count)
+                {
+                    //if (running == 2)
+                    //    break;
+                    while (udpc.Available <= 0)
+                    {
+                        if (timeout > 10000)
+                            break;
+                        if (running > 1)
+                            break;
+                        System.Threading.Thread.Sleep(1);
+                        if (first != 1)
+                            timeout++;
+                        if ((timeout % 1000) == 0)
+                            a = udpcs.Send(sendbuffer, 7);
+                    }
+                    if (timeout > 10000)
+                        break;
+                    if (running > 1)
+                        break;
+                    timeout = 0;
+                    buffer = udpc.Receive(ref ipe);
+					// primi 32 bit numero blocco
+                    received_sequence_number = (uint)buffer[0] + (uint)buffer[1] * 256 + (uint)buffer[2] * 256 * 256 + (uint)buffer[3] * 256 * 256 * 256;
+					// poi 16 bit numero pacchetto
+                    received_packet_number = (uint)buffer[4] + (uint)buffer[5] * 256;
+                    if (first == 1)
+                    {
+                        last = (ulong)received_sequence_number * (ulong)receive_count + (ulong)received_packet_number;
+                        first = 0;
+                    }
+                    else
+                    {
+                        now = (ulong)received_sequence_number * (ulong)receive_count + (ulong)received_packet_number;
+                        if (now >= last)
+                            diff = now - last;
+                        else
+                        {
+                            diff = 0xffffffffffffffff - last + now + 1;
+                            wrap++;
+                        }
+                        //differ[diff] |= 128;
+                        last = now;
+                        missing = missing + diff - 1;
+                    }
+                    if (received_sequence_number == sequence_number_next0)
+                    {
+                        if (expected == received_packet_number)
+                        {
+                            expected++;
+                            //fil.Write(buffer, 0, buffer.GetUpperBound(0) + 1);
+                        }
+                        else
+                        {
+							expected = (int)received_packet_number + 1;
+                            /*running = 3;*/
+                        }
+                        /*receivedblock[0]--;*/
+                        if (save_data == 1)
+                            fil.Write(buffer, 0, buffer.GetUpperBound(0) + 1);
+                    }
+                    else if (received_sequence_number == sequence_number_next1)
+                    {
+						break;
+                        /*receivedblock[1]--;*/
+                        fil.Write(buffer, 0, buffer.GetUpperBound(0) + 1);
+                    }/*
+                    else
+                    {
+                    }*/
+                    received++;
+                }
+                if (timeout > 10000)
+                    break;
+                if (running > 1)
+                    break;
+                sequence_number++;
+                sequence_number_next0++;
+                sequence_number_next1++;
+            }
+            if (running > 1)
+            {
+                System.Threading.Thread.Sleep(1000);
+                while (udpc.Available > 0)
+                {
+                    buffer = udpc.Receive(ref ipe);
+                }
+                sendbuffer[6] = 128;
+                if (running == 3)
+                    sendbuffer[6] |= 64;
+                a = udpcs.Send(sendbuffer, 7);
+            }
+            udpcs.Close();
+            udpc.Close();
+            if (save_data == 1)
+                fil.Close();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int pps;
+            pps = (int)(received - recvlast);
+            recvlast = received;
+            this.Text = "Form1 - " + System.Convert.ToString(received) + " - " + System.Convert.ToString(missing) + " - " + System.Convert.ToString(pps) + "Hz - " + System.Convert.ToString((pps*1400)/(1024*1024)) + " " + System.Convert.ToString(wrap);
+        }
+    }
+}
